54515ea835ad059176a443206650ef921fd1e483
shagun <shagun0621@gmail.com>
2024-01-09 17:56:52 +0530

    added

---
+++ index.js
@@ -0,0 +1,15 @@
+function isPrime(num){
+    let count=0
+    for(let i=1;i<=num;i++){
+        if(num%i===0){ 
+            count++
+        }
+    }
+    if(count!==2){
+        return true
+    }
+    else{
+        return false
+    }
+}
+console.log(isPrime(13));
